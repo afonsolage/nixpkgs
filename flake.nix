4a488a3a27b47cf262f17bb1aaf54e6f69902d01
Afonso Lage <lage.afonso@gmail.com>
2024-03-17 17:24:43 -0300

    Added neovim nightly

--- flake.nix
+++ flake.nix
@@ -6,16 +6,20 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 	home-manager.url = "github:nix-community/home-manager/release-23.11";
 	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, neovim-nightly-overlay, ... }: 
  	let
 		lib = nixpkgs.lib;
 		lib-hm = home-manager.lib;
 		system = "x86_64-linux";
-		pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+		pkgs = import nixpkgs { 
+            inherit system; 
+            overlays = [neovim-nightly-overlay.overlay];
+            config.allowUnfree = true; 
+        };
 		pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-
 	in {
 		nixosConfigurations = {
 			afonso-pc = lib.nixosSystem {
